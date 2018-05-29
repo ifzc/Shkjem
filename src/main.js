465e57150fecd0bd15169c79f978fd96eca36c8b
Source # <Smileioc@163.com>
2018-05-29 10:13:14 +0800

    优化IE兼容性

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// ES6 兼容组件
+import 'babel-polyfill';
+import promise from 'es6-promise';
+promise.polyfill();
+
 import Vue from 'vue'
 import App from './App'
 import router from './router/router'
@@ -5,17 +10,13 @@ import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/zh-CN'
 import VideoPlayer from 'vue-video-player'
 import http from './router/http'
+
 import './../static/js/jquery-1.8.3.min'
 import './../static/js/main'
 import "video.js/dist/video-js.css"
 import "vue-video-player/src/custom-theme.css"
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
 import 'animate.css'
-
-// ES6 兼容组件
-import 'babel-polyfill';
-import promise from 'es6-promise';
-promise.polyfill();
 
 // 服务器地址
 Vue.prototype.serverpath = 'http://120.79.76.250/api/'
